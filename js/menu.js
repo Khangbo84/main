2db16913fa2629caae8446b63aa0bd70e2643f8a
K Games Legends <160814799+Khangbo84@users.noreply.github.com>
2026-07-17 16:45:57 +0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,16 +5,16 @@
         function goToPage(page) {
             switch (page) {
                 case 'maps':
-                    window.location.href = "khangbo84.github.io/main/html/map.html";
+                    window.location.href = "https://khangbo84.github.io/main/html/map.html";
                     break;
                 case 'runestone':
-                    window.location.href = "https://example.com/runestone";
+                    window.location.href = "https://khangbo84.github.io/main/html/runestones.html";
                     break;
                 case 'games':
-                    window.location.href = "https://example.com/games";
+                    window.location.href = "https://khangbo84.github.io/main/html/games";
                     break;
                 case 'events':
-                    window.location.href = "https://example.com/events";
+                    window.location.href = "https://khangbo84.github.io/main/html/events";
                     break;
             }
         }
